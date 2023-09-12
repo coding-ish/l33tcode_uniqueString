e7f36a1bb68e3098bb96f45b7b093bdd98894008
imendez <ishmendez18@gmail.com>
2023-09-12 15:36:00 -0500

    try

--- app/src/main/java/com/example/uniquestring/MainActivity.kt
+++ app/src/main/java/com/example/uniquestring/MainActivity.kt
@@ -7,5 +7,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println(Solution().firstUniqChar("test"))
+    }
+}
+
+class Solution {
+    fun firstUniqChar(s: String): Int {
+        var firstUnique = -1
+        for(i in 0 until s.length){
+            println(s.substring(i, i+1))
+            if(!s.contains(s.substring(i, i+1))){
+                firstUnique = i
+                break
+            }
+        }
+        return firstUnique
     }
 }
